982b7ba827613d7066d55448a26f5f5ac9356c7b
unknown <galina.zalogin@gmail.com>
2024-05-11 22:10:29 +0300

    updated changes

--- src/components/pages/sign/SignUp.js
+++ src/components/pages/sign/SignUp.js
@@ -9,6 +9,7 @@ import Navbar from "../../navbar/Navbar";
 import { useNavigate  } from 'react-router-dom';
 import { FaEye, FaEyeSlash} from 'react-icons/fa';
 import { useEffect,useRef,useState } from "react";
+import {countryList} from './SignUpLists'
 
 export default function Checking() {
     const [companyID, setCompanyID] = useState('');
@@ -101,7 +102,7 @@ export default function Checking() {
                                 required/>
                         </div> */}
                         <div className="form-group">
-                            <label for="email">Email : </label>
+                            <label for="email">Email</label>
                             <input
                                 type="email" 
                                 id="email"
@@ -111,7 +112,7 @@ export default function Checking() {
                                 required />
                         </div>
                         <div className="form-group">
-                            <label for="password">Password : 
+                            <label for="password">Password 
                             {/* <div  className="show-password"> */}
                             <i onClick={togglePasswordVisiblity}>{passwordShown ?  <FaEye/>:<FaEyeSlash/>}</i>
                             {/* </div> */}
@@ -128,7 +129,7 @@ export default function Checking() {
                         </div>
                      
                             <div className="form-group">
-                            <label for="organization name">Organization's name : </label>
+                            <label for="organization name">Organization's name</label>
                             <input 
                                type="text"
                                id="organization name"
@@ -139,24 +140,34 @@ export default function Checking() {
                                required/>
                         </div>
                         <div className="form-group">
-                            <label for="organization domain">Organization's domain : </label>
-                            <input type="text" 
+                            <label for="organization domain">Organization's domain</label>
+                            {/* <input type="text" 
                                id="organization domain"
                                name="domain" 
                                placeholder="Organization's domain"
                                value={domain} 
                                onChange={(e) => setDomain(e.target.value)}
-                               required/>
+                               required/> */}
+                                <select class="form-select" aria-label="Default select example" value ={domain} defaultValue={"Health"} onChange={(e)=>setDomain(e.target.value)}>
+                                {/* <option selected>Open this select menu</option> */}
+                                <option value="Health" >Health</option>
+                                <option value="Industry" >Industry</option>
+                                <option value="Economy" >Economy</option>
+                                <option value="High tech" >High tech</option>
+                                <option value="Transport" >Transport</option>
+                                <option value="Education" >Education</option>
+                                </select  >
                         </div>
                         <div className="form-group">
-                            <label for="date of establishment">Date of establishment : </label>
-                            <input type="date"
-                                id= "date of establishment"
+                            <label for="Year of establishment">Year of establishment</label>
+                            <input type="number"
+                                id= "Year of establishment"
                                name="establishment" 
-                               placeholder="Date of establishment of the company" 
+                               placeholder="Year of establishment of the company" 
                                value={establishment} 
                                onChange={(e) => setEstablishment(e.target.value)}
                                required/>
+
                         </div>
                         </div>
                         <div className="register-container-child">
@@ -171,36 +182,40 @@ export default function Checking() {
                                required/>
                         </div> */}
                         <div className="form-group">
-                        <label for="occupation">The organization is local or global : </label>
+                        <label for="occupation">The organization is local or global</label>
                         <select class="form-select" aria-label="Default select example" value ={occupation} defaultValue={"Local"} onChange={(e)=>setOccupation(e.target.value)}>
                         {/* <option selected>Open this select menu</option> */}
+                
                         <option value="Local" >Local</option>
                         <option value="Global" >Global</option>
                         </select  >
                         
                         </div>
                         <div className="form-group">
-                            <label for="location">The organization’s location : </label>
-                            <input type="text" 
-                                id="location"
-                               name="location" 
-                               placeholder="The organization’s location" 
-                               value={location} 
-                               onChange={(e) => setLocation(e.target.value)} 
-                               required/>
+                        <label htmlFor="location">Branch location</label>
+                        <select className="form-select" aria-label="Default select example" value={location} onChange={(e) => setLocation(e.target.value)}>
+                        {console.log("countries", countryList)}
+                        {countryList.map(element => (
+                            <option key={element} value={element}>{element}</option>
+                        ))}
+                        </select>
+
                         </div>
                         <div className="form-group">
-                            <label for="organization size">Organization’s size : </label>
-                            <input type="number" 
-                                id="organization size"
-                               name="size" 
-                               placeholder="Organization’s size" 
-                               value={size} 
-                               onChange={(e) => setSize(e.target.value)} 
-                               required min="1"/>
+                            <label for="organization size">Organization’s size</label>
+                            <select class="form-select" aria-label="Default select example" value ={size} defaultValue={"Windows"} onChange={(e)=>setSize(e.target.value)}>
+                            <option value="1-50">1-50</option>
+                            <option value="51-200" >51-200</option>
+                            <option value="200-500" >200-500</option>
+                            <option value="501+" >501+</option>
+
+                        </select  >
+
                         </div>
-                        <div className="form-group">
-                            <label for="num of ceo">Number of Deputy CEOs : </label>
+                        {/* not relevant maybe in the futher */}
+
+                        {/* <div className="form-group">
+                            <label for="num of ceo">Number of Deputy CEOs</label>
                             <input type="number" 
                                 id= "num of ceo"
                                name="numOfCeo" 
@@ -210,7 +225,7 @@ export default function Checking() {
                                required min="1"/>
                         </div>
                         <div className="form-group">
-                            <label for="amount of managers">Amount of managers : </label>
+                            <label for="amount of managers">Amount of managers</label>
                             <input type="number"
                                 id="amount of managers"
                                name="numOfManagers"
@@ -220,22 +235,23 @@ export default function Checking() {
                                required min="1" />
                         </div>
                         <div className="form-group">
-                            <label for="Amount of employees">Amount of regular employees : </label>
+                            <label for="Amount of employees">Amount of regular employees</label>
                             <input type="number" 
                                 id="Amount of employees"
                                name="numOfEmployees" 
                                placeholder="Amount of regular employees" 
                                value={numOfEmployees} 
                                onChange={(e) => setNumOfEmployees(e.target.value)} 
-                               required min="1"/>
-                        </div>
-                        <div className="form-group">
-                            <label for="organization system used">Organization’s operation system used : </label>
+                               required min="1"/> */}
+                        {/* </div> */}
+                        {/* <div className="form-group">
+                            <label for="organization system used">Organization’s operation system used</label>
                             <select class="form-select" aria-label="Default select example" value ={systemUsed} defaultValue={"Windows"} onChange={(e)=>setSystemUsed(e.target.value)}>
                             <option value="Windows" >Windows</option>
                             <option value="Linux" >Linux</option>
+                            <option value="macOS" >macOS</option>
                         </select  >
-                        </div>
+                        </div> */}
                         </div>
                     </div>
                     <div className="btn" >
